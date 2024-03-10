b182d84c2175c139db447ebffee43bfc6011f302
steffen-brown <steffenbrown1@gmail.com>
2024-03-10 10:53:39 -0500

    PIC functions completed

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -11,16 +11,84 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+    uint8_t cache1, cache2;
+    uint32_t flags;
+
+    // Disable interupts and save flags
+    cli_and_save(flags);
+
+    // Save current mask
+    cache1 = inb(MASTER_8259_DATA_PORT);
+    cache2 = inb(SLAVE_8259_DATA_PORT);
+
+    // Mask both of the PICs
+    outb(0xFF, MASTER_8259_DATA_PORT);
+    outb(0xFF, SLAVE_8259_DATA_PORT);
+
+    // Start the intialization seqence (cascasde mode)
+    outb(ICW1, MASTER_8259_PORT);
+    outb(ICW1, SLAVE_8259_PORT);
+
+    // Set the PICs vector offsets
+    outb(ICW2_MASTER, MASTER_8259_PORT);
+    outb(ICW2_SLAVE, SLAVE_8259_PORT);
+
+    // Tell master PIC ther eis a slave at IRQ2
+    outb(ICW3_MASTER, MASTER_8259_PORT);
+    // Tell the slave its in cascade mode
+    outb(ICW3_SLAVE, SLAVE_8259_PORT);
+
+    // Enable 8086 mode on the PICs
+    outb(ICW4, MASTER_8259_PORT);
+    outb(ICW4, SLAVE_8259_PORT);
+
+    // Restore masks
+    outb(cache1, MASTER_8259_DATA_PORT);
+    outb(cache2, SLAVE_8259_DATA_PORT);
+
+    // Restore interupt flags
+    restore_flags(flags);
 }
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
+    uint8_t new_mask;
+
+    // If IRQ is connected to the primary PIC (IRQ < 8)
+    if(irq_num < 8) {
+        // Create and output new primary PIC mask w/ new enable bit
+        new_mask = inb(MASTER_8259_DATA_PORT) | (1 << irq_num);
+        outb(new_mask, MASTER_8259_DATA_PORT);
+    } else { // If IRQ is connected to the secondary PIC (IRQ >= 8)
+        // Create and output new secondary PIC mask w/ new enable bit
+        new_mask = inb(SLAVE_8259_DATA_PORT) | (1 << (irq_num - 8));
+        outb(new_mask, SLAVE_8259_DATA_PORT);
+    }
 }
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
+    uint8_t new_mask;
+
+    // If IRQ is connected to the primary PIC (IRQ < 8)
+    if(irq_num < 8) {
+        // Create and output new primary PIC mask w/ new disabled bit
+        new_mask = inb(MASTER_8259_DATA_PORT) & ~(1 << irq_num);
+        outb(new_mask, MASTER_8259_DATA_PORT);
+    } else { // If IRQ is connected to the secondary PIC (IRQ >= 8)
+        // Create and output new secondary PIC mask w/ new disabled bit
+        new_mask = inb(SLAVE_8259_DATA_PORT) & ~(1 << (irq_num - 8));
+        outb(new_mask, SLAVE_8259_DATA_PORT);
+    }
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
+    // If IRQ is connected to the secondary PIC (IRQ >= 8)
+    if(irq_num >= 8) {
+        // Send EOI to secondary PIC
+        outb(SLAVE_8259_PORT, EOI);
+    }
+    // Send EOI to primary PIC
+    outb(MASTER_8259_PORT, EOI);
 }
