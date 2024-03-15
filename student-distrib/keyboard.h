d4f6af10a1d524ff989bcc4ea9a32bc6e9e7b45b
AdityaGupta1492 <adityagupta1492@gmail.com>
2024-03-14 22:21:20 -0400

    Keyboard Comments + Edits

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -5,14 +5,16 @@
 #include "i8259.h"
 #include "lib.h"
 
-#define SCAN_CODES  58
-#define BUFFER_SIZE 128
+#define SCAN_CODES  58  // number of scan codes used
+#define BUFFER_SIZE 128  // size of screen keyboard buffer
 
 char keyboard_buffer[BUFFER_SIZE];
-
 extern char scan_codes_table[SCAN_CODES];
 
+/* initializes keyboard interrupt on the PIC */
 void keyboard_init(void);
+
+/* handles keyboard interrupts and prints typed input to screen */
 void keyboard_handler(void);
 
 #endif
