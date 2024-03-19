42ad1d4c751e36fe02bcd467f71312b937846a8e
AdityaGupta1492 <adityagupta1492@gmail.com>
2024-03-18 20:06:03 -0500

    Shift and Caps Lock almost working with one bug

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,16 +1,33 @@
 #include "keyboard.h"
-#define keyboard_port 0x60
 
 // Directory of letters assocated with each scan code
 char scan_codes_table[SCAN_CODES] = {
     NULL, NULL, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', NULL,      
-    NULL, 'q', 'w', 'e', 'r', 't', 'y', 'u',  'i', 'o', 'p', '[', ']', NULL,
+    ' ', 'q', 'w', 'e', 'r', 't', 'y', 'u',  'i', 'o', 'p', '[', ']', NULL,
     NULL, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', NULL, '\\',     
     'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', NULL,
     NULL, NULL, ' ',
 };
 
+char scan_codes_table_shift[SCAN_CODES] = {
+    NULL, NULL, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', NULL,      
+    ' ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U',  'I', 'O', 'P', '{', '}', NULL,
+    NULL, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~', NULL, '|',     
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', NULL,
+    NULL, NULL, ' ',
+};
+
+char scan_codes_table_caps[SCAN_CODES] = {
+    NULL, NULL, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', NULL,      
+    ' ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U',  'I', 'O', 'P', '[', ']', NULL,
+    NULL, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '`', NULL, '\\',   
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', NULL,
+    NULL, NULL, ' ',
+};
+
 static int keyboard_index; // global variable for keyboard buffer index
+static int shift_flag;
+static int caps_lock_flag;
 
 /*
  * keyboard_init
@@ -23,6 +40,8 @@ static int keyboard_index; // global variable for keyboard buffer index
 void keyboard_init(void) {
     enable_irq(1); // keyboard interrupt = IRQ1
     keyboard_index = 0;
+    shift_flag = 0;
+    caps_lock_flag = 0;
 }
 
 /*
@@ -34,14 +53,38 @@ void keyboard_init(void) {
  *   SIDE EFFECTS: Prints input to screen
  */
 void keyboard_handler(void) {
-    uint8_t scan_code = inb(keyboard_port) & 0xFF; // take in first 8 bits of keyboard input
+    uint8_t scan_code = inb(KEYBOARD_PORT) & 0xFF; // take in first 8 bits of keyboard input
+
+    if (scan_code == LEFT_SHIFT || scan_code == RIGHT_SHIFT) {
+        shift_flag = 1;
+    } else if (scan_code == LEFT_SHIFT_REL || scan_code == RIGHT_SHIFT_REL) {
+        shift_flag = 0;
+    } 
+    
+    if (scan_code == CAPS_LOCK && !caps_lock_flag) {
+        caps_lock_flag = 1;
+    } else if (scan_code == CAPS_LOCK && caps_lock_flag) {
+        caps_lock_flag = 0;
+    }
+
+    printf("Shift Flag: %d, Caps Lock Flag: %d, Scan Code: %d\n\n", shift_flag, caps_lock_flag, scan_code);
 
     // Filter out invalid or otherwise unhandled scancodes
-    if (scan_code < SCAN_CODES) {
-        keyboard_buffer[keyboard_index] = scan_codes_table[scan_code]; // get matching character for scan_code
-        if(keyboard_buffer[keyboard_index]) {
-            putc(keyboard_buffer[keyboard_index]); // print to screen
+    if (scan_code < SCAN_CODES && scan_codes_table[scan_code]) {
+        if (shift_flag && caps_lock_flag) {
+            keyboard_buffer[keyboard_index] = scan_codes_table[scan_code]; // get matching character for scan_code
+            printf("0 ");
+        } else if (shift_flag && !caps_lock_flag) {
+            keyboard_buffer[keyboard_index] = scan_codes_table_shift[scan_code]; // get matching character for scan_code
+            printf("1 ");
+        } else if (caps_lock_flag && !shift_flag) {
+            keyboard_buffer[keyboard_index] = scan_codes_table_caps[scan_code]; // get matching character for scan_code
+            printf("2 ");
+        } else {
+            keyboard_buffer[keyboard_index] = scan_codes_table[scan_code]; // get matching character for scan_code
+            printf("3 ");
         }
+        putc(keyboard_buffer[keyboard_index]); // print to screen
         keyboard_index++; // Advance the keyboard buffer index
     }
 
--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -5,11 +5,16 @@
 #include "i8259.h"
 #include "lib.h"
 
-#define SCAN_CODES  58  // number of scan codes used
-#define BUFFER_SIZE 128  // size of screen keyboard buffer
+#define SCAN_CODES      58      // number of scan codes used
+#define BUFFER_SIZE     128     // size of screen keyboard buffer
+#define KEYBOARD_PORT   0x60    // keyboard data port
+#define LEFT_SHIFT      0x2A    // left shift scan code
+#define RIGHT_SHIFT     0x36    // right shift scan code
+#define LEFT_SHIFT_REL  0xAA    // left shift released scan code
+#define RIGHT_SHIFT_REL 0xB6    // right shift released scan code 
+#define CAPS_LOCK       0x3A    // caps lock scan code
 
 char keyboard_buffer[BUFFER_SIZE];
-extern char scan_codes_table[SCAN_CODES];
 
 /* initializes keyboard interrupt on the PIC */
 void keyboard_init(void);
